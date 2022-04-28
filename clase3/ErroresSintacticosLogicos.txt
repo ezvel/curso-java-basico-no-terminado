89dd781b553694b0e7356b8492cd9bd1c653f325
ezvelq <lvelezezequiel7@gmail.com>
2022-04-28 17:02:54 -0300

    Clase 3 - Java - Errores sintáctico y lógicos

---
+++ clase3/ErroresSintacticosLogicos.txt
@@ -0,0 +1,13 @@
+/*
+Clase 3
+Errores sintácticos y lógicos
+
+Siempre el nombre de la clase comenzar con mayúsculas
+
+*/
+
+public class ErroresSintacticosLogicos{
+  public static void main(String args[]){
+   System.out.println("Hello world!!!!");
+ }
+}
---
+++ clase3/erroresSintacticosLogicos.java
@@ -0,0 +1,46 @@
+/*
+Clase 3
+Errores sintácticos y lógicos
+
+Siempre el nombre de la clase comenzar con mayúsculas
+
+Hay dos tipos de errores
+Error lógico -> Sintáctico del lenguaje
+Error semántico -> Error lógico del programador. El programa no hace lo que el programador quiere
+
+*/
+
+public class ErroresSintacticosLogicos{
+  public static void main(String args[]){
+   System.out.println("Hello world!!!!");
+ }
+}
+
+/*
+REFLEXIONES
+Considero que hay x cosas que hacen a un buen desarrollador
+
+1. Buena actitud frente a los errores o problemas
+Los problemas o errores a los que nos enfrentamos nos proporciona la oportunidad de mejorar
+las habilidades y además es algo con el que el programador vive día a con día. Por lo tanto es mejor
+acostumbrarse a ello. Tiende a haber la idea de que los curso pueden a realizarse a velocidades
+exorbitantes cómo una serie de Netflix y cuánto más rápido lo termino mejor. Por experiencia, los curso que se hagan y las clases que se avancen es abstracto, solo vive en la pantalla, uno no es mejor ni peor por cuánto a durado en hacer un curso online. El proósito del curso es aprender, lo que marca un antes y un después en la vida del programador es aprender independientemente del tiempo en el que llevó. Por lo tanto, cuánto se tarda un curso y si lo veo en velocidades duplicadas es abstracto y solo vive en las pantallas. SIn embargo, el aprendizaje es concreto y vive en la mente del programador.
+
+Es decir, es importante ir poco, detenidamente, avanzando y exponerse a los errores que van surgiendo, y no cometer el error de seguir avanzando sin resolver esos problemas o sin entenderlos, debido que lo que perdura es el aprendizaje y no que tan rápido se "avanzó"
+
+Para concretar, los errores y los problemas a lo que el programador se enfrenta es una oportunidad para mejorar las habilidades y aprender, por lo tanto, por medio de la experiencia y así poder, porque no, impulsar la carrera del programador. Eso es un motivo suficiente para desarrollar una buena actitud frente a ello y, además, creo que las convicciones profundas en base a motivos convicentes generan un cambio voluntario en la actitud y en el comportamiento. Por lo tanto, conociendo este motivo de la oportunidad en los errores para el crecimiento del programador, creo que, mejorará la actitud a cuándo estos lleguen.
+
+2. Aprender a leer los errores
+En el curso se dice "Aprender a leer los errores facilita la programación". No todo al programar es escribir código, sino resolver errores y problemas. Entonces, aprender a leer los errores es fundamental.
+Por eso es tan importante el inglés
+
+3. Saber inglés
+Los errores están en inglés, si por cada error voy a buscarlo en el traductor de Google, se pierde mucho tiempo
+
+4. Aprender a buscar en Google
+Aprender a buscar en Google cómo colocar el nombre del error y saber en que sitios buscarlo cómo en StackOverFlow es fundamental. Alguien decía que un buen programador es un buen buscador en Google
+
+5. Entender de fondo el problema
+Si sabes leer el problema y buscar la solución, tienes que entender los conceptos subyacentes que abarcan ese problema y los conceptos subyacentes que abarca la solución. Conceptos computacionales, del lenguaje propio o del sistema operativo. Es decir, no nos quedemos solamente con "copiar y pegar" y lo socucioné, opino que es imporante entender que sucede por detras para así, entonces, mejorar las habilidades del programador.
+
+*/
